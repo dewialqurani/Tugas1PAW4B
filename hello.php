0a856f885eab0691949060bd2458ccd0c1210d0d
dewialqurani <imanialqurani158@gmail.com>
2022-03-08 23:45:23 +0700

    menambahkan comentar di file hello.php

--- hello.php
+++ hello.php
@@ -4,7 +4,7 @@
     <title>Soal Nomor 1</title>
 </head>
 <body>
-    <h1>Nama : <?php echo "Dewi Imani Al Qur' Ani";?><h1>
+    <h1>Nama : <?php echo "Dewi Imani Al Qur' Ani";?><h1>//disini termasuk php didalam html. Fungsi Echo sendiri untuk menampilkan teks ke layar. 
     <h1>Nim : <?php echo "200411100014";?><h1>
 </body>
 </html>
