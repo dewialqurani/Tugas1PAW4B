c33548708cd002dcb25ca2ac18b6de60f9ccf04b
dewialqurani <imanialqurani158@gmail.com>
2022-03-08 23:50:14 +0700

    menambahkan comentar di file variable.php

--- variable.php
+++ variable.php
@@ -4,18 +4,18 @@
     <title>Soal Nomor 2</title>
 </head>
 <body>
-    <h3>Nama : <?php echo "Dewi Imani Al Qur' Ani";?><h3>
+    <h3>Nama : <?php echo "Dewi Imani Al Qur' Ani";?><h3> //disini termasuk php didalam html. Fungsi Echo sendiri untuk menampilkan teks ke layar. 
     <h3>Nim : <?php echo "200411100014";?><h3>
     <?php
-    $num1 = 8;
-    $num2 = 7;
-    $num3 = 6;
+    $num1 = 8; //membuat variabel pertama
+    $num2 = 7; //membuat variabel kedua
+    $num3 = 6; //membuat variabel ketiga
 
-    $hasil1=$num1 +$num2 +$num3;
+    $hasil1=$num1 +$num2 +$num3; //menambah variabel baru untuk melihat hasil dari operasi 3 variabel pertama 
     $hasil2=$num1 +$num2 -$num3;
     $hasil3=$num1 *$num2 /$num3;
 
-    echo"Hasil Operasi Matematika yang Pertama adalah $hasil1 <br>";
+    echo"Hasil Operasi Matematika yang Pertama adalah $hasil1 <br>"; //menampilkan hasil operasi matematika ke layar
     echo"Hasil Operasi Matematika yang Kedua adalah $hasil2 <br>";
     echo"Hasil Operasi Matematika yang Ketiga adalah $hasil3 <br>";
     ?>
