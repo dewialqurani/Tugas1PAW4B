c773b4c6c781b1ae2ee2869ab921b7c130f137e8
dewialqurani <imanialqurani158@gmail.com>
2022-03-08 23:24:19 +0700

    Menambahkan file hello.php

---
+++ hello.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Soal Nomor 1</title>
+</head>
+<body>
+    <h1>Nama : <?php echo "Dewi Imani Al Qur' Ani";?><h1>
+    <h1>Nim : <?php echo "200411100014";?><h1>
+</body>
+</html>
